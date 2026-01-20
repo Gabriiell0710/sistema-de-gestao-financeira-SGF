9f2b4b4e0270d103b68b3f9a262b4ada4f9fca3c
Gabriel Carvalho <gabriiell0710@gmail.com>
2026-01-20 13:04:57 -0300

    feat: User model created and Conext created

---
+++ SGF/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SGF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGF.Data
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<UserModel> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string PastaApp = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "GestaoFinanceira"
+            );
+
+            string CaminhoDb =
+            Path.Combine(PastaApp, "financeiro.db");
+
+            optionsBuilder.UseSqlite( CaminhoDb );
+        }
+    }
+}
